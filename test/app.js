1a5cfeb01d35367ea854bf6b5573a92ad87e1c2f
wang zuo <wzuoadjusted@gmail.com>
2015-12-16 22:49:29 +0800

    <ButtonExample/>

--- test/app.js
+++ test/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './button.example';
+import ButtonExample from './button.example';
 
-ReactDOM.render(<Example/>, document.getElementById('app'));
+ReactDOM.render(<ButtonExample/>, document.getElementById('app'));
